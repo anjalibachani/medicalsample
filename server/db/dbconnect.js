2e1e86539d22494166a8d0d7734ee3e29f88cd01
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-10-19 15:11:00 -0400

    added fasted field to samples

--- server/db/dbconnect.js
+++ server/db/dbconnect.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 const config = require('../config/config.json');
 
-const db= mysql.createConnection({
-    host : config.database.host,
-    user : config.database.user,
-    password : config.database.password,
-    database : config.database.database
+const db = mysql.createConnection({
+  host: config.database.host,
+  user: config.database.user,
+  password: config.database.password,
+  database: config.database.database,
+  dateStrings:true
 });
 
 db.connect((err)=>{
