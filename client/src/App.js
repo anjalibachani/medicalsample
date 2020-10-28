b29f466f160cd6b0d7fc525e954ad5b0de421740
rohit <vkintali@buffalo.edu>
2020-10-27 21:12:24 -0400

    App.js changes

--- client/src/App.js
+++ client/src/App.js
@@ -12,12 +12,11 @@ import CustomHeaderButton from "./components/CustomHeaderButton";
 import AddSamples from "./components/AddSamples";
 import forgotpass from './components/forgotpass';
 import resetpass from './components/resetpass';
-<<<<<<< HEAD
+
 //import Logout from './components/Logout';
 import AddShipments from './components/AddShipments';
-=======
+
 import Logout from './components/Logout'
->>>>>>> 7b520e1aa6afbd909def2d02c8a48a9a52859d8f
 
 function App() {
   return (
@@ -29,11 +28,7 @@ function App() {
           <Route path="/AddSamples" component={AddSamples} />
           <Route path="/forgot-pass" component={forgotpass} />
           <Route path="/reset-pass/:id" component={resetpass} />
-<<<<<<< HEAD
-          {/*<Route path="/logout" component={Logout}/>*/}
-=======
           <Route path="/logout" component={Logout}/>
->>>>>>> 7b520e1aa6afbd909def2d02c8a48a9a52859d8f
           {/* <Route path="/ViewSamples" component={ViewSamples} />
           <Route path="/Reports" component={Reports} />
           <Route path="/ViewShipments" component={ViewShipments} />*/}
